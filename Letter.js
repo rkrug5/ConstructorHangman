88e3b08095dfe94fa843bdaef25fb0af4a52b4b5
rkrug5 <phishermankrug@hotmail.com>
2018-04-10 17:16:27 -0700

    hw things

--- Letter.js
+++ Letter.js
@@ -6,36 +6,27 @@ var letterGuess = process.argv[2];
 
 
 
-function Letter(letterValue, guessed) {
+function Letter(letterValue) {
 
 	this.letterValue = letterValue;
 
 	//i think i might want to set this one initially to false since all of
 	//my letters start of as unguessed...
-	this.guessed = guessed;
+	this.guessed = false;
 
 
 
-	// this.toString = function () {
-	// 	if (this.guessed === false) {
-	// 		console.log("_");
-	// 	} else {
-	// 		console.log("this.letterValue");
-	// 	}
+}
 
-	// };
-
-	this.checker = function () {
-		if (letterGuess === this.letterValue) {
-			this.guessed = true;
-		}
-
+Letter.prototype.checker = function () {
+	if (letterGuess === this.letterValue) {
+		this.guessed = true;
 	}
 
 }
 
 
-Letter.prototype.toString = function () {
+Letter.prototype.reveal = function () {
 	if (this.guessed === false) {
 		console.log("_");
 	} else {
@@ -52,7 +43,7 @@ var a = new Letter("a", true);
 // var c = new Letter("c", false);
 
 
-console.log(a.toString());
+// console.log(a.toString());
 
 
 module.exports = Letter;
--- Word.js
+++ Word.js
@@ -1,13 +1,61 @@
+
 var Letter = require("./Letter");
 
-var inquirer = require("inquirer");
-
-function Word() {
-
-	let wordLetters = [];
+// var inquirer = require("inquirer");
+var word = new Word("apples");
+// var solutionChars = hangmanSolution.split('');
 
 
+// console.log(solutionChars);
+
+function Word(word) {
+
+	this.solutionLetters = [];
 
 
+	this.word = word;
+
+	for (var i = 0; i < word.length; i++) {
+		this.solutionLetters.push(new Letter(word[i]));
+
+	}
+
+	// console.log(this.solutionLetters);
 
 }
+
+Word.prototype.toString = function () {
+
+	var display = '';
+
+	for (var i = 0; i < this.solutionLetters.length; i++) {
+
+		display += this.solutionLetters[i].reveal() + ' ';
+
+	}
+
+	// console.log(display);
+	// console.log('\n' + display + '\n\n');
+}
+
+
+Word.prototype.checkerWord = function () {
+	for (var i = 0; i < this.solutionLetters.length; i++) {
+
+		this.solutionLetters[i].checker();
+	}
+}
+
+word.toString();
+
+
+
+
+
+
+
+
+
+
+module.exports = Word;
+
